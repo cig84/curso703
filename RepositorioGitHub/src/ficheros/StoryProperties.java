1199dfbea72650cd4fb9ab0187228ecbac16fdfb
cig84 <cig84@hotmail.it>
2015-05-12 21:00:34 +0200

    Actualización de StoryProperties

--- RepositorioGitHub/src/ficheros/StoryProperties.java
+++ RepositorioGitHub/src/ficheros/StoryProperties.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import java.util.Properties;
 
 public class StoryProperties {
